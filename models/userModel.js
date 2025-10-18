dafc045c73ea854a5bfe7958546e47193f9fdedf
Andy Trinh <akt7120@utexas.edu>
2025-10-18 16:05:27 -0500

    new branch

--- models/userModel.js
+++ models/userModel.js
@@ -3,10 +3,10 @@ const UserSchema = {
   name: String,
   password: String,
   birthday: String,
-  userSpecific: {
-    income: Number,
-    creditScore: Number
-  },
+ 
+  income: Number,
+  creditScore: Number,
+  
   vehicleCondition: String, // 'NEW' or 'USED'
   fuelType: String, // 'GAS', 'HYBRID', or 'ELECTRIC'
   vehicleYearRange: Array // [minYear, maxYear]
